50155a02e9d906a7f3e4e3c61b5c450a31821c2c
Geovanna Toledo Vieira Gonçalves <geovanna.vieira.goncalves@escola.pr.gov.br>
2026-06-02 11:38:35 +0000

    .

--- main.php
+++ main.php
@@ -1,4 +1,52 @@
 <?php
+class Pessoa{
+    public $nome;
+    public $idade;
 
-echo "Olá, mundo!" . PHP_EOL;
-echo "PHP versão: " . PHP_VERSION . PHP_EOL;
+  public function __construct($nome, $idade){
+    $this->nome = $nome;
+    $this->idade = $idade;
+  }
+
+  public function apresentar(){
+    return "Olá, meu nome é $this->nome e tenho $this->idade anos de idade.\n";
+   }
+
+   public function verificarIdade(){
+     // if($this->idade >= 18){
+     //   return "$this->nome é maior de idade.\n";
+     // }else{
+     //   return "$this->nome é menor de idade.\n";
+     // }
+     
+     return ($this->idade >= 18) ? "$this->nome é maior de idade.\n" : "$this->nome é menor de idade.\n";
+   }
+}
+
+// $Ana = new Pessoa("Ana", 28);
+// $Bruno = new Pessoa("Bruno", 16);
+// $Carla = new Pessoa("Carla", 21);
+// $Diego = new Pessoa("Diego", 14);
+// $Eduarda = new Pessoa("Eduarda", 21);
+
+
+// echo $Ana->apresentar();
+// echo $Bruno->apresentar();
+// echo $Carla->apresentar();
+// echo $Diego->apresentar();
+// echo $Eduarda->apresentar();
+
+// echo $Ana->verificarIdade();
+// echo $Bruno->verificarIdade();
+// echo $Carla->verificarIdade();
+// echo $Diego->verificarIdade();
+// echo $Eduarda->verificarIdade();
+//echo "$Ana->apresentar()";
+
+$nomes = ["Ana", "Bruno", "Carla", "Diego", "Eduarda"];
+$idades = [28, 16, 21, 14, 21];
+
+for($i = 0; $i <= 4; $i++){
+  ${$nomes[$i]} = new Pessoa($nomes[$i], $idades[$i]);
+}
+?>
